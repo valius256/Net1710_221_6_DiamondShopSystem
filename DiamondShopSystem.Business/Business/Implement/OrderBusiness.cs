5907a6d89a056c508931c6bf23e27a02f0ea7191
Quang Phat <68989339+valius256@users.noreply.github.com>
2024-05-18 17:05:57 +0700

    feature add GetAllOrder() and GetById() (#1)

---
+++ DiamondShopSystem.Business/Business/Implement/OrderBusiness.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiamondShopSystem.Business.Business.Interfaces;
+using DiamondShopSystem.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiamondShopSystem.Business.Business.Imp
+{
+
+
+    public class OrderBusiness : IOrderBusiness
+    {
+        private readonly Net1710_221_6_DiamondShopSystemContext _context;
+
+        public async Task<List<Order>> GetAllOrder()
+        {
+            return await _context.Orders.ToListAsync();
+        }
+
+        public async Task<Order> getOrderByIdTask(int id)
+        {
+            return await _context.Orders.FirstOrDefaultAsync(x => x.OrderId == id);
+        }
+    }
+}
