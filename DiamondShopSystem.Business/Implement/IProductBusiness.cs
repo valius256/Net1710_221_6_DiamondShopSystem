80195bddab586f9526e531ac705ff71d875d64dd
kisphapha <thanhhung16082003@gmail.com>
2024-05-18 17:12:15 +0700

    Move file

--- DiamondShopSystem.Business/Implement/IProductBusiness.cs
+++
@@ -1,9 +0,0 @@
-﻿using DiamondShopSystem.Data.Models;
-
-namespace DiamondShopSystem.Business.Implement
-{
-    public interface IProductBusiness
-    {
-        Task<List<Product>> GetAllProducts();
-    }
-}
