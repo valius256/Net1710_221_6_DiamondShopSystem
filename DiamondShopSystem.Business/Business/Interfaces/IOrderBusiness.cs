86ad1e3403da7f48cf739635398da3c31aab28ed
Quang Phat <quangphat7a1@gmail.com>
2024-05-18 17:03:20 +0700

    feature add GetAllOrder() and GetById()

---
+++ DiamondShopSystem.Business/Business/Interfaces/IOrderBusiness.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiamondShopSystem.Data.Models;
+
+namespace DiamondShopSystem.Business.Business.Interfaces
+{
+    public interface IOrderBusiness
+    {
+
+        Task<List<Order>> GetAllOrder();
+        Task<Order> getOrderByIdTask(int id);
+    }
+}
