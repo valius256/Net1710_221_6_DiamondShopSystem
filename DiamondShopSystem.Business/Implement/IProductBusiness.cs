17879e8dd019b4fec3533bc71a00d190c482aa24
kisphapha <thanhhung16082003@gmail.com>
2024-05-18 17:05:54 +0700

    product business

---
+++ DiamondShopSystem.Business/Implement/IProductBusiness.cs
@@ -0,0 +1,9 @@
+﻿using DiamondShopSystem.Data.Models;
+
+namespace DiamondShopSystem.Business.Implement
+{
+    public interface IProductBusiness
+    {
+        Task<List<Product>> GetAllProducts();
+    }
+}
