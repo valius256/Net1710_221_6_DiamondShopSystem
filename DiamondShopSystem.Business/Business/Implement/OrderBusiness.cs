3a84d750ec4ce1504d7033a6a65f6f6eb47e8a89
Quang Phat <68989339+valius256@users.noreply.github.com>
2024-05-21 20:50:35 +0700

    phat_feature_CRUD (#3)

--- DiamondShopSystem.Business/Business/Implement/OrderBusiness.cs
+++ DiamondShopSystem.Business/Business/Implement/OrderBusiness.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DiamondShopSystem.Business.Business.Interfaces;
+﻿using DiamondShopSystem.Business.Business.Interfaces;
 using DiamondShopSystem.Data.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -23,6 +18,28 @@ namespace DiamondShopSystem.Business.Business.Imp
         public async Task<Order> getOrderByIdTask(int id)
         {
             return await _context.Orders.FirstOrDefaultAsync(x => x.OrderId == id);
+        }
+
+        public async Task<Order> CreateOrderTask(Order order)
+        {
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+            return order;
+        }
+
+        public async Task<Order> UpdateOrderTask(Order order)
+        {
+            _context.Orders.Update(order);
+            await _context.SaveChangesAsync();
+            return order;
+        }
+
+        public async Task<Order> DeleteOrderTask(int id)
+        {
+            var order = await _context.Orders.FirstOrDefaultAsync(x => x.OrderId == id);
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+            return order;
         }
     }
 }
