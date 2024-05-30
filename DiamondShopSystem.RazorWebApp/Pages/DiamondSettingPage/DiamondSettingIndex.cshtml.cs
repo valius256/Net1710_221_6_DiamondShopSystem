8a13743babe098b7e0a270a48dc37ffacfb166d6
Jancaster1234 <122957619+Jancaster1234@users.noreply.github.com>
2024-05-30 21:51:42 +0700

    Phuc: display on web these tables: customer, main diamond, diamond setting, side stone

---
+++ DiamondShopSystem.RazorWebApp/Pages/DiamondSettingPage/DiamondSettingIndex.cshtml.cs
@@ -0,0 +1,30 @@
+using DiamondShopSystem.Business.Business.Interfaces;
+using DiamondShopSystem.Business.Business.Implement;
+using DiamondShopSystem.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DiamondShopSystem.RazorWebApp.Pages.DiamondSettingPage
+{
+    public class DiamondSettingModel : PageModel
+    {
+        private readonly IDiamondSettingBusiness _diamondSettingBusiness;
+        public DiamondSettingModel(IDiamondSettingBusiness diamondSettingBusiness)
+        {
+            _diamondSettingBusiness = diamondSettingBusiness;
+        }
+        public IEnumerable<DiamondSetting> DiamondSettings { get; set; } = new List<DiamondSetting>();
+
+        public async Task OnGetAsync()
+        {
+            var result = await _diamondSettingBusiness.GetAllDiamondSettings();
+            DiamondSettings = result.Data != null ? (List<DiamondSetting>)result.Data : new List<DiamondSetting>();
+
+        }
+
+        public void OnPost()
+        {
+
+        }
+    }
+}
