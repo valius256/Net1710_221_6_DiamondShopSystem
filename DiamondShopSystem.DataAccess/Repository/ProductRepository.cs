006ba7792e5cfba7ff246f50383611827518499f
kisphapha <thanhhung16082003@gmail.com>
2024-06-05 15:15:25 +0700

    Add getting product navigator

--- DiamondShopSystem.DataAccess/Repository/ProductRepository.cs
+++ DiamondShopSystem.DataAccess/Repository/ProductRepository.cs
@@ -1,9 +1,18 @@
 ﻿using DiamondShopSystem.Data.Models;
 using DiamondShopSystem.DataAccess.Base;
+using Microsoft.EntityFrameworkCore;
 
 namespace DiamondShopSystem.DataAccess.Repository
 {
     public class ProductRepository : GenericRepository<Product>
     {
+        public async Task<List<Product>> GetProducts()
+        {
+            return await _context.Products
+                .Include(p => p.MainDiamond)
+                .Include(p => p.SideStone)
+                .Include(p => p.DiamondSetting)
+                .ToListAsync();
+        }
     }
 }
