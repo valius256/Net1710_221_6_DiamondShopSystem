fa44ade105708ebdfa335766db5b08bb862813de
Quang Phat <68989339+valius256@users.noreply.github.com>
2024-06-05 15:10:25 +0700

    phat push Order wpf (#26)

---
+++ Net1710_221_6_DiamondShopSystem.WpfApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Net1710_221_6_DiamondShopSystem.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
