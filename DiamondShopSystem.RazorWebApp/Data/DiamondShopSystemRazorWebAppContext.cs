4c2908dd33709f4f8a2b11908ae294ca3c9dccfd
kisphapha <thanhhung16082003@gmail.com>
2024-06-12 15:26:01 +0700

    lesson hom truoc #2

---
+++ DiamondShopSystem.RazorWebApp/Data/DiamondShopSystemRazorWebAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DiamondShopSystem.Data.Models;
+
+namespace DiamondShopSystem.RazorWebApp.DataAccess
+{
+    public class DiamondShopSystemRazorWebAppContext : DbContext
+    {
+        public DiamondShopSystemRazorWebAppContext (DbContextOptions<DiamondShopSystemRazorWebAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DiamondShopSystem.Data.Models.Product> Product { get; set; } = default!;
+    }
+}
---
+++ DiamondShopSystem.RazorWebApp/Pages/ProductPage/Delete.cshtml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DiamondShopSystem.Data.Models;
+using DiamondShopSystem.RazorWebApp.DataAccess;
+using DiamondShopSystem.Business.Business.Implement;
+using DiamondShopSystem.Business.Business.Interfaces;
+
+namespace DiamondShopSystem.RazorWebApp.Pages.ProductPage
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly IProductBusiness _productBusiness;
+        private readonly IMainDiamondBusiness _mainDiamondBusiness;
+        private readonly IDiamondSettingBusiness _diamondSettingBusiness;
+        private readonly ISideStoneBusiness _sideStoneBusiness;
+        public DeleteModel(IProductBusiness productBusiness, IMainDiamondBusiness mainDiamondBusiness, IDiamondSettingBusiness diamondSettingBusiness, ISideStoneBusiness sideStoneBusiness)
+        {
+            _productBusiness = productBusiness;
+            _mainDiamondBusiness = mainDiamondBusiness;
+            _diamondSettingBusiness = diamondSettingBusiness;
+            _sideStoneBusiness = sideStoneBusiness;
+        }
+
+        [BindProperty]
+        public Product Product { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = (await _productBusiness.GetByIdAsync((int)id)).Data as Product;
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Product = product;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = (await _productBusiness.GetByIdAsync((int)id)).Data as Product;
+            if (product != null)
+            {
+                Product = product;
+                await _productBusiness.DeleteProduct((int)id);
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
